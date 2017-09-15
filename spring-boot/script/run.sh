6a947aaa9c82aa83760072ae265cfa26bbac8788
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-09-15 16:20:15 +0200

    style: code refactoring

--- spring-boot/script/run.sh
+++ spring-boot/script/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-httpversion=$1
-serverport=$2
 java -Xbootclasspath/p:lib/alpn-boot-8.1.5.v20150921.jar  \
     -jar target/servlet4-demo-spring-boot-0.0.1-SNAPSHOT.jar  \
-    --http.version=$httpversion --server.port=$serverport \
     --debug
--- spring-boot/src/main/java/de/holisticon/servlet4demospringboot/GreetingController.java
+++ spring-boot/src/main/java/de/holisticon/servlet4demospringboot/GreetingController.java
@@ -3,6 +3,7 @@ package de.holisticon.servlet4demospringboot;
 import de.holisticon.servlet4demospringboot.dto.Greeting;
 import org.eclipse.jetty.server.PushBuilder;
 import org.eclipse.jetty.server.Request;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +18,21 @@ public class GreetingController {
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
 
+  /**
+   *
+   * @see org.springframework.web.servlet.mvc.method.annotation.ServletRequestMethodArgumentResolver
+   * @param request
+   * @param name
+   * @return
+   */
   @RequestMapping("/greeting")
   public Greeting greeting(ServletRequest request, @RequestParam(value = "name", defaultValue = "World") String name) {
-
     PushBuilder pushBuilder = Request
         .getBaseRequest(request)
         .getPushBuilder();
-
     pushBuilder
         .path("/push-greeting?name=push")
         .push();
-
     return new Greeting(counter.incrementAndGet(),
                         String.format(template, name));
   }
