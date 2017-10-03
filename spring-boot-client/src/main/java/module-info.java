157691145892bbd0d855f45d08c0ff1df54ab1f2
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-10-03 21:58:38 +0200

    style: order imports in module-info.java

--- spring-boot-client/src/main/java/module-info.java
+++ spring-boot-client/src/main/java/module-info.java
@@ -9,17 +9,17 @@
   requires spring.aop;
   requires spring.web;
   requires spring.expression;
-
   requires spring.boot;
   requires spring.boot.autoconfigure;
+
   requires log4j.over.slf4j;
   requires http2.client;
-  requires jetty.client;
-  requires okhttp;
   requires http2.http.client.transport;
-  requires jetty.util;
   requires http2.common;
+  requires jetty.client;
   requires jetty.http;
+  requires jetty.util;
+  requires okhttp;
   requires jackson.annotations;
 
   exports de.holisticon.servlet4demospringbootclient;
