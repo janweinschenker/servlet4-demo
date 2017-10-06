df3d51e18536f7d0323b679d12c905df0c57ac5a
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-10-06 09:29:02 +0200

    chore: store surefire reports. Fix path to test reports directory.

--- spring-boot-client/src/main/java/module-info.java
+++ spring-boot-client/src/main/java/module-info.java
@@ -1,13 +1,6 @@
-@SuppressWarnings({"deprecation", "removal"})
-    module de.holisticon.servlet4demospringbootclient {
-
-  //requires java.se.ee;
+module de.holisticon.servlet4demospringbootclient {
 
   requires spring.core;
-  requires spring.beans;
-  requires spring.context;
-  requires spring.aop;
-  requires spring.web;
   requires spring.expression;
   requires spring.boot;
   requires spring.boot.autoconfigure;
@@ -15,6 +8,11 @@
   requires okhttp;
   requires jackson.annotations;
   requires log4j.over.slf4j;
+  requires spring.beans;
+  requires spring.context;
+  requires  spring.web;
+
+
 
   exports de.holisticon.servlet4demojettyclient;
 }
