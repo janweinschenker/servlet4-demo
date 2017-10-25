f397d81fd2515e3cbe659a3c7227721d3e04b667
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-10-25 11:25:48 +0200

    feat: add tomcat server w/ h2 support

---
+++ servlet4-demo-spring-boot-tomcat-server/src/main/java/de/holisticon/servlet4demospringboot/Application.java
@@ -0,0 +1,25 @@
+package de.holisticon.servlet4demospringboot;
+
+import org.apache.coyote.http2.Http2Protocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public TomcatServletWebServerFactory tomcatCustomizer() {
+    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+    factory.addConnectorCustomizers((connector -> {
+      connector.addUpgradeProtocol(new Http2Protocol());
+    }));
+    return factory;
+  }
+}
+
