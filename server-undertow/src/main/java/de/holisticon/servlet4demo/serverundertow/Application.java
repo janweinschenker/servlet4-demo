4e68e289c0536b4709f88d47517f1de4ae572a9e
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-10-25 12:52:25 +0200

    feat: add maven resource plugin

--- server-undertow/src/main/java/de/holisticon/servlet4demo/serverundertow/Application.java
+++ server-undertow/src/main/java/de/holisticon/servlet4demo/serverundertow/Application.java
@@ -1,13 +1,20 @@
 package de.holisticon.servlet4demo.serverundertow;
 
 import io.undertow.UndertowOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
 public class Application {
+
+  @Value("${server.port.http}")
+  public int httpPort;
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
