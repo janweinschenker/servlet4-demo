449f7fbe6c9840f674e049f696ae46a00e81b258
Jan Weinschenker <jan.weinschenker@holisticon.de>
2017-11-03 14:35:41 +0100

    chore: remove unused import

--- server-undertow/src/test/java/de/holisticon/servlet4demo/serverundertow/ApplicationTest.java
+++ server-undertow/src/test/java/de/holisticon/servlet4demo/serverundertow/ApplicationTest.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.when;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
